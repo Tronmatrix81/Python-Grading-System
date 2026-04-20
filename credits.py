51148c14ba4978143aa8d45662b595c740303b19
Tronmatrix81 <arrobajustie@gmail.com>
2026-04-20 11:14:45 -0600

    credits screen

---
+++ credits.py
@@ -0,0 +1,9 @@
+import os
+
+# Clear screen
+os.system("cls")
+
+print("\t~~ MIEMBROS DE TRANZIT ~~")
+print("\n\tJustie Ariel Loya Martinez")
+print("\n\tJosue Caleb Escobedo Herrera")
+print("\n\tErik Gallo Gardea")
