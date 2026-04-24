082df9a09cb85ac92316a1fbf99f18d08ea45393
Tronmatrix81 <arrobajustie@gmail.com>
2026-04-23 20:23:57 -0600

    Added menu and try-catch

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-if __name__=="__main__":
-    print("Hello World!")
+import os
+import studentsearch
+
+menuCommands = ["1) Search Student ID", "2) Something"]
+    
+def menu():
+    os.system('cls')
+    print("UACH Grading System")
+    for i in range(0, len(menuCommands)):
+        print(menuCommands[i])
+
+    try:
+        option = int(input())
+    except ValueError:
+        menu()
+    else:     
+        match (option):
+            case 1:
+                studentsearch.SearchForStudent()
+            case 2:
+                print()
+        menu()
+
+
+if __name__=="__main__": menu()
--- studentsearch.py
+++ studentsearch.py
@@ -1,4 +1,13 @@
+import os
 from team import students
+
 
 def FindStudent(num: int)-> str:
     return students.get(num, "Student not found!")
+
+def SearchForStudent():
+    os.system("cls")
+    print("Type in a student ID: ")
+    id = int(input())
+    print(FindStudent(id), "\n")
+    os.system('pause')
