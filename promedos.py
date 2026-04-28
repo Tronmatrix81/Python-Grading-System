aad175a578a15e303390ae57b3938e3797c49651
Skirul <skirul2709@gmail.com>
2026-04-27 23:38:06 -0600

    Update promedos.py

--- promedos.py
+++ promedos.py
@@ -3,7 +3,10 @@ import studentsearch
 matricula=0
 aux1=0
 aux2=0
+queso=""
 nombres=(0,0)
+rep=0
+apro=0
 def saved(mat: int):
     studentsearch.FindStudent
     print("Insert your Student ID")
@@ -12,6 +15,8 @@ def saved(mat: int):
     aux1=matricula
     print(aux1)
     queso=students.get(aux1,"hola")
+    quesoo=queso
+
 def promedio():
     subjects=int(input("How many subjects do you have? "))
     aux2=subjects
@@ -20,7 +25,21 @@ def promedio():
         print("The ammount of subjects is less or more than the necessary to continue.")
     else:
         subjects=0
-        for i in range(aux2):
+    
+        materia=input("What is the name of the subject that you want to calculate the average?")
+        p1=int(input("What is the grade that you got in your first partial? "))
+        p2=int(input("What is the grade that you got in your second partial? "))
+        p3=int(input("What is the grade that you got in your third partial? "))
+        prom=((p1*0.3)+(p2*0.3)+(p3*0.4))
+        
+        print("Your average in ", materia," is: ", prom )
+        if prom <7:
+            rep+=1
+            print("You have failed this subject")
+        if prom <10 and prom >=7:
+            apro+=1
+            print("You have passed this subject!")
+
 
             
 
