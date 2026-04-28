bb5308fafd3fc89a9b01f23016683ba4ed4e7299
Skirul <skirul2709@gmail.com>
2026-04-28 00:05:44 -0600

    Update promedos.py

--- promedos.py
+++ promedos.py
@@ -12,35 +12,61 @@ def saved(mat: int):
     matricula=int(input())
     print(studentsearch.FindStudent(matricula))
     aux1=matricula
+    
     queso=students.get(aux1,"Student not found!")
     quesoo=queso
     if queso == "Student not found!":
         return
     else:
         promedio()
+
+
 def promedio():
-    subjects=int(input("How many subjects do you have? "))
-    aux2=subjects
-    #necesito total reprobadas, aprobadas y total de materias en dif variables
-    if subjects >12 or subjects<2:
-        print("The ammount of subjects is less or more than the necessary to continue.")
-    else:
-        subjects=0
+    casos=int(input("Do you want to calculate just one subject or all your subjects?" \
+    " 1- JUST ONE SUBJECT" \
+    " 2- ALL SUBJECTS "))
     
-        materia=input("What is the name of the subject that you want to calculate the average?")
-        p1=int(input("What is the grade that you got in your first partial? "))
-        p2=int(input("What is the grade that you got in your second partial? "))
-        p3=int(input("What is the grade that you got in your third partial? "))
-        prom=((p1*0.3)+(p2*0.3)+(p3*0.4))
-        rep=0
-        apro=0
-        print("Your average in ", materia," is: ", prom )
-        if prom <7:
-            rep+=1
-            print("You have failed this subject")
-        if prom <10 and prom >=7:
-            apro+=1
-            print("You have passed this subject!")
+    match casos:
+        case 1:
+            materia=input("What is the name of the subject that you want to calculate the average?")
+            p1=int(input("What is the grade that you got in your first partial? "))
+            p2=int(input("What is the grade that you got in your second partial? "))
+            p3=int(input("What is the grade that you got in your third partial? "))
+            prom=((p1*0.3)+(p2*0.3)+(p3*0.4))
+            rep=0
+            apro=0
+            print("Your average in ", materia," is: ", prom )
+            if prom <7:
+                rep+=1
+                print("You have failed this subject")
+            if prom <10 and prom >=7:
+                apro+=1
+                print("You have passed this subject!")
+
+
+        case 2:  #necesito total reprobadas, aprobadas y total de materias en dif variables
+            subjects=int(input("How many subjects do you have in total?"))
+            aux2=subjects
+            if subjects >12 or subjects<2:
+                print("The ammount of subjects is less or more than the necessary to continue.")
+            else:
+                subjects=0
+                for i in range(aux2):
+                    materia=input("What is the name of the subject that you want to calculate the average?")
+                    p1=int(input("What is the grade that you got in your first partial? "))
+                    p2=int(input("What is the grade that you got in your second partial? "))
+                    p3=int(input("What is the grade that you got in your third partial? "))
+                    prom=((p1*0.3)+(p2*0.3)+(p3*0.4))
+                    rep=0
+                    apro=0
+                    print("Your average in ", materia," is: ", prom )
+                    if prom <7:
+                        rep+=1
+                        print("You have failed this subject")
+                    if prom <10 and prom >=7:
+                        apro+=1
+                        print("You have passed this subject!")
+
         
 
 
